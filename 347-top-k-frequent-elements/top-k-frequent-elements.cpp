a27535cc940faf82bef8cb00a968336b7ac9204f
Aditya4288 <142107058+Aditya4288@users.noreply.github.com>
2025-08-17 20:52:02 +0530

    Time: 6 ms (20.28%) | Memory: 17.8 MB (96.01%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // Use hash map to store frequency of every number
-        unordered_map<int, int> count;
-        for (int i = 0; i < nums.size(); i++) {
-            count[nums[i]]++;
+        // Hash map to count frequencies of each number
+        unordered_map<int, int> freq;
+
+        // Count frequencies
+        for (int& num : nums)
+            freq[num]++; // Increment frequency count for each number
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            minHeap;
+        for (auto& pair : freq) {
+            int num = pair.first;
+            int count = pair.second;
+            minHeap.push({count, num});
+            if (minHeap.size() > k)
+                minHeap.pop();
         }
 
-        // Copy the hash map to a vector of pair and sort
-        vector<pair<int, int>> vec(count.begin(), count.end());
-        sort(vec.begin(), vec.end(), [](const auto& a, const auto& b) {
-            return a.second > b.second;
-        });
-
-        // Build result vector
         vector<int> result;
-        for (int i = 0; i < k; i++) {
-            result.push_back(vec[i].first);
+        while (!minHeap.empty()) {
+            result.push_back(minHeap.top().second);
+            minHeap.pop();
         }
+
         return result;
     }
 };
